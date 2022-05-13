313e8b5e90f4bbee4645f135ee054017bd2778da
Rioni <nrosa01@ucm.es>
2022-05-13 18:53:36 +0200

    Detectar cambio de controlador en el Input

--- Parasite/Assets/Scripts/ScriptablesObjects/Input/InputReader.cs
+++ Parasite/Assets/Scripts/ScriptablesObjects/Input/InputReader.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Events;
 using System;
+using UnityEngine.InputSystem.LowLevel;
+public enum DeviceType { Keyboard, Mouse, Gamepad, Unknown };
 
 [CreateAssetMenu(fileName = "InputReader", menuName = "Game/Input Reader")]
 
@@ -16,8 +18,11 @@ public class InputReader : ScriptableObject, GameInput.IGameplayActions
     public event Action finishMoveEvent;
     public event Action dashEvent;
     public event Action pauseEvent;
+    public event Action<DeviceType> deviceChangedEvent;
 
     private GameInput gameInput;
+    private DeviceType currentDeviceType = DeviceType.Mouse;
+    InputDevice currentDevice;
 
     private void OnEnable()
     {
@@ -30,12 +35,52 @@ public class InputReader : ScriptableObject, GameInput.IGameplayActions
             gameInput.Gameplay.SetCallbacks(this);
         }
         EnableGameplayInput();
+        InputSystem.onEvent += OnInputEvent;
+    }
+
+    private void OnInputEvent(InputEventPtr args, InputDevice device)
+    {
+        if (this.currentDevice != device)
+        {
+            DeviceType deviceType = GetDeviceType(device);
+
+            if (HasChangedDevice(deviceType))
+                deviceChangedEvent?.Invoke(currentDeviceType);
+
+            currentDeviceType = deviceType;
+            this.currentDevice = device;
+        }
+    }
+
+    // If currentDeviceType is Mouse and device is Keyboard, return false, return false also is currentDeviceType is Keyboard and device is Mouse
+    bool HasChangedDevice(DeviceType newDeviceType)
+    {
+        if (currentDeviceType == DeviceType.Mouse && newDeviceType == DeviceType.Keyboard)
+            return false;
+        else if (currentDeviceType == DeviceType.Keyboard && newDeviceType == DeviceType.Mouse)
+            return false;
+        else
+            return true;
+    }
+
+    private DeviceType GetDeviceType(InputDevice device)
+    {
+        if (device is Mouse)
+            return DeviceType.Mouse;
+        else if (device is Gamepad)
+            return DeviceType.Gamepad;
+        else if (device is Keyboard)
+            return DeviceType.Keyboard;
+
+        return DeviceType.Unknown;
     }
 
     private void OnDisable()
     {
         DisableAllInput();
         attackAction?.Dispose();
+        attackRangedAction?.Dispose();
+        InputSystem.onEvent -= OnInputEvent;
     }
 
     public void OnAttackMelee(InputAction.CallbackContext context)
@@ -54,8 +99,8 @@ public class InputReader : ScriptableObject, GameInput.IGameplayActions
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        if(moveEvent != null && context.phase == InputActionPhase.Performed) moveEvent?.Invoke(context.ReadValue<Vector2>());
-        if(finishMoveEvent != null && context.phase == InputActionPhase.Canceled) finishMoveEvent?.Invoke();
+        if (moveEvent != null && context.phase == InputActionPhase.Performed) moveEvent?.Invoke(context.ReadValue<Vector2>());
+        if (finishMoveEvent != null && context.phase == InputActionPhase.Canceled) finishMoveEvent?.Invoke();
     }
 
     public void OnPause(InputAction.CallbackContext context)
